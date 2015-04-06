bc83356612f6e6932b63c8cfce02335d40a642cd
Park <ppark@it-p6-l-nmiller.cmg.int>
2015-04-06 16:28:39 -0400

    added sign up form

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -26,6 +26,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
